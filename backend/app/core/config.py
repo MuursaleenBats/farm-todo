84fdc971001a06b87561d13db6454993d524ce71
MuursaleenBats <mursaleenbatatawala@gmail.com>
2023-07-19 17:26:55 +0530

    config file setup done

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,5 +1,22 @@
-from pydantic import BaseSettings
+from typing import List
+from pydantic import AnyHttpUrl
+from pydantic_settings import BaseSettings
 from decouple import config
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     JWT_SECRET_KEY: str = config("JWT_SECRET_KEY", cast=str)
+    JWT_REFRESH_SECRET_KEY: str = config("JWT_REFRESH_SECRET_KEY", cast=str)
+    ALGORITHM = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
+    REFRESH_ACCESS_TOKEN_EXPIRE_MINUTES: int = 60*24*7 #7 DAYS
+    BACKEND_CORS_ORIGIN: List[AnyHttpUrl] = []
+    PROJECT_NAME: str = "TODOLIST"
+
+    #Database Settings
+    MONGO_CONN: str = config("MONGO_CONN",cast=str)
+
+    class Config:
+        case_sensitive = True
+
+settings = Settings()
+
