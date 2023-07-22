39c577dfe10ef4c7d953e5442a56da744517a0e4
MuursaleenBats <mursaleenbatatawala@gmail.com>
2023-07-22 12:15:02 +0530

    jwt authentication added

--- backend/app/api/api_v1/router.py
+++ backend/app/api/api_v1/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from app.api.api_v1.handler import user
-
+from app.api.auth.jwt import auth_router
 
 router = APIRouter()
 router.include_router(user.user_router,prefix = "/user", tags= ["user"])
+router.include_router(auth_router, prefix='/auth', tags = ['auth'])
--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -1,6 +1,7 @@
 from app.schemas.user_schemas import UserAuth
 from app.models.user_model import User
-from app.core.security import get_password
+from app.core.security import get_password, verify_pass
+from typing import Optional
 
 class UserService:
     @staticmethod
@@ -13,3 +14,19 @@ class UserService:
 
         await user_in.save()
         return user_in
+    
+    @staticmethod
+    async def authenticate(email: str, password: str)-> Optional[User]:
+        user = await UserService.get_user_by_email(email=email)
+        if not user:
+            return None
+        if not verify_pass(password=password, hashed_pass=user.hashed_password):
+            return None
+        
+        return user
+
+
+    @staticmethod
+    async def get_user_by_email(email: str) -> Optional[User]:
+        user = await User.find_one(User.email == email)
+        return user
